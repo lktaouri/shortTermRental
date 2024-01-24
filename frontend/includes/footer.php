4a7dc800935cac7f010d67f1d2c0b2a391aeef2a
Arnold Kovacs <k.arnold789@gmail.com>
2024-01-24 18:08:13 +0100

    footer geändert

--- frontend/includes/footer.php
+++ frontend/includes/footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="footer d-flex justify-content-center"> <!-- hinzugefügt: d-flex und justify-content-center für Flexbox-Layout -->
-        <p class="mr-5"><a href="about.php">About us</a></p> <!-- hinzugefügt: mr-3 für rechten Abstand zwischen den Links -->
-        <p class="mr-5"><a href="contact.php">Contact</a></p>
+        <p class="mr-5"><a href="about.php">Über uns</a></p> <!-- hinzugefügt: mr-3 für rechten Abstand zwischen den Links -->
+        <p class="mr-5"><a href="contact.php">Kontakt</a></p>
         <p class="mr-5"><a href="impressum.php">Impressum</a></p>
     </div>
 </footer>
